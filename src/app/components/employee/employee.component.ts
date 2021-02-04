ced64f55f3476099746c0b9e305b498721710513
Akhlaque <akhlaquea01@gmail.com>
2021-02-04 23:15:25 +0530

    updated email

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -16,8 +16,8 @@ export class EmployeeComponent {
           this.employee = [
               {empId:'10001',fname:'Firoz',lname:'Khan',dep:'Web Devloper', city:'Bengaluru',email:'khan3firoz@gmail.com'},
               {empId:'10002',fname:'Akhlaque',lname:'Ahmad',dep:'Web Devloper', city:'Bengaluru',email:'akhlaquea01@gmail.com'},
-              {empId:'10003',fname:'Ashfaque',lname:'Ahmad',dep:'Data Scientist', city:'Bengaluru',email:'ashfaque786@gmail.com'},
-              {empId:'10004',fname:'Krishna',lname:'kumar',dep:'Mechanical', city:'Noida',email:'twariji@gmail.com'},
+              {empId:'10003',fname:'Ashfaque',lname:'Ahmad',dep:'Data Scientist', city:'Bengaluru',email:'ashfaque386@gmail.com'},
+              {empId:'10004',fname:'Krishna',lname:'kumar',dep:'Mechanical', city:'Noida',email:'twariji0700@gmail.com'},
 
           ]
       }
